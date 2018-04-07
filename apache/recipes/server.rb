e1128c0a7905d05675b5c371acfc980231980c2c
EC2 Default User <ec2-user@ip-172-31-12-56.ap-south-1.compute.internal>
2018-04-07 09:52:15 +0000

    Added template to apache cookbook

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -4,15 +4,10 @@ service 'httpd' do
     action [:enable, :start]
 end
 
-file '/var/www/html/index.html' do
-    content "<html> <title>Welcome</title><h1> This is an Apache webserver installed via Chef receipe</h1> 
-             <body> Hostname = #{node['hostname']} <br>
-                    Ipaddress = #{node['ipaddress']} <br>
-                    Total Memory = #{node['memory']['total']} <br>
-                    Cpu Size = #{node['cpu']['0']['mhz']}
-"
-    mode "655"
+template '/var/www/html/index.html' do
+    source 'index.html.erb' 
     action :create
+    variables(:name => 'Prashant')
 end
 
 
